9d41b5660d7ebd2b95350109c2d84a5c5d4eaa74
Tyler Ketron <tketron@gmail.com>
2018-12-03 18:05:56 -0800

    revert template

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 var express = require('express');
-const Themeparks = require('themeparks');
 var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    Output: 'Hello World!'
-  });
-});
-
-app.get('/test', (req, res) => {
-  const park = new Themeparks.Parks[disneylandPark]();
-  res.send({
-    park: park
+    "Output": "Hello World!"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    Output: 'Hello World!'
+    "Output": "Hello World!"
   });
 });
 
+
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app;
+module.exports = app
