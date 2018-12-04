033d76c37df3daf83e10b1b8e0af3c5d0242d671
Tyler Ketron <tketron@gmail.com>
2018-12-03 18:07:47 -0800

    resetting app file

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
+const Themeparks = require('themeparks');
 var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Theme Parks Go Here!'
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Hello World!'
   });
 });
 
-
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
